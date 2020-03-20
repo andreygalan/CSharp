19573273eb4bfa54f863f419488a6cb703650678
andreygalan <42898805+andreygalan@users.noreply.github.com>
2020-03-20 10:13:10 +0300

    Add files via upload

---
+++ WindowsFormsApp6/WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.V = new System.Windows.Forms.RichTextBox();
+            this.R = new System.Windows.Forms.RichTextBox();
+            this.VR = new System.Windows.Forms.RichTextBox();
+            this.L = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // V
+            // 
+            this.V.Location = new System.Drawing.Point(52, 193);
+            this.V.Name = "V";
+            this.V.Size = new System.Drawing.Size(141, 146);
+            this.V.TabIndex = 0;
+            this.V.Text = "";
+            // 
+            // R
+            // 
+            this.R.Location = new System.Drawing.Point(199, 193);
+            this.R.Name = "R";
+            this.R.Size = new System.Drawing.Size(137, 146);
+            this.R.TabIndex = 1;
+            this.R.Text = "";
+            // 
+            // VR
+            // 
+            this.VR.Location = new System.Drawing.Point(342, 193);
+            this.VR.Name = "VR";
+            this.VR.Size = new System.Drawing.Size(142, 146);
+            this.VR.TabIndex = 2;
+            this.VR.Text = "";
+            // 
+            // L
+            // 
+            this.L.Location = new System.Drawing.Point(490, 193);
+            this.L.Name = "L";
+            this.L.Size = new System.Drawing.Size(143, 146);
+            this.L.TabIndex = 3;
+            this.L.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "см верш";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(243, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "см ребер";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(380, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "верш и реб";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(509, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "список смежности";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(673, 373);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(325, 373);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "DFS",
+            "BFS"});
+            this.comboBox1.Location = new System.Drawing.Point(262, 36);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(167, 21);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(73, 373);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.L);
+            this.Controls.Add(this.VR);
+            this.Controls.Add(this.R);
+            this.Controls.Add(this.V);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox V;
+        private System.Windows.Forms.RichTextBox R;
+        private System.Windows.Forms.RichTextBox VR;
+        private System.Windows.Forms.RichTextBox L;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp6/WindowsFormsApp6/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void prV()
+        {
+            String[] str = V.Lines;
+
+            int n;
+            n = str.Length;
+
+            int[,] matr = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                String[] tmp = str[i].Split(' ');
+                for (int j = 0; j < n; j++)
+                    matr[i, j] = Convert.ToInt32(tmp[j]);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                //L.AppendText((i+1).ToString()+":");
+                for (int j = 0; j < n; j++)
+                    if (matr[i, j] == 1) L.AppendText(" " + (j + 1).ToString());
+                L.AppendText("\n");
+            }
+            //инцендентность
+            int col = 0;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    if (Convert.ToBoolean(matr[i, j])) col++;
+            col /= 2;
+            int[,] matrinc; 
+            matrinc = createVR(matr, col, n);
+            createR(matrinc, col, n);
+        }
+
+
+
+
+        private void prL()
+        {
+            String[] str = L.Lines;
+            List<List<int>> list = new List<List<int>>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                List<int> a = new List<int>();
+                String[] tmp = str[i].Split(' ');
+                for (int j = 0; j < tmp.Length; j++)
+                    a.Add(Convert.ToInt32(tmp[j]));
+                list.Add(a);
+            }
+            int[,] matr = new int[list.Count, list.Count];
+
+
+            for (int i = 0; i < list.Count; i++)
+                for (int j = 0; j < list.Count; j++)
+                    matr[i, j] = 0;
+
+            for (int i = 0; i < list.Count; i++)
+                for (int j = 0; j < list[i].Count; j++)
+                {
+                    matr[i, list[i][j] - 1] = 1;
+                    matr[list[i][j] - 1, i] = 1;
+                }
+
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int j = 0; j < list.Count; j++)
+                    V.AppendText(matr[i, j].ToString() + " ");
+                V.AppendText("\n");
+            }
+            int col = 0;
+            int n = list.Count;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    if (Convert.ToBoolean(matr[i, j])) col++;
+            col /= 2;
+            int[,] matrinc;
+            
+            matrinc = createVR(matr, col, n);
+
+            createR(matrinc, col, n);
+
+
+        }
+
+        private void prVR()
+        {
+            String[] str = VR.Lines;
+            int n = str.Length;
+            int m = str[0].Split(' ').Length;
+            int[,] matrinc = new int[n, m];
+            int[,] matr = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                String[] tmp = str[i].Split(' ');
+                for (int j = 0; j < m; j++)
+                    matrinc[i, j] = Convert.ToInt32(tmp[j]);
+            }
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    matr[i, j] = 0;
+            int k = 0;
+            for (int i = 0; i < m; i++)
+            {
+                k = 0;
+                while (!Convert.ToBoolean(matrinc[k, i]))
+                    k++;
+                for (int j = k + 1; j < n; j++)
+                {
+
+                    if (Convert.ToBoolean(matrinc[k, i]) && Convert.ToBoolean(matrinc[j, i]))
+                    {
+                        matr[k, j] = 1;
+                        matr[j, k] = 1;
+
+                    }
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    V.AppendText(matr[i, j].ToString() + " ");
+                V.AppendText("\n");
+            }
+            for (int i = 0; i < n; i++)
+            {
+                //L.AppendText((i+1).ToString()+":");
+                for (int j = 0; j < n; j++)
+                    if (matr[i, j] == 1) L.AppendText(" " + (j + 1).ToString());
+                L.AppendText("\n");
+            }
+            createR(matrinc, m, n);
+        }
+        private void createR(int[,] matrinc, int col, int n)
+        {
+            int[,] matrR = new int[col, col];
+            for (int i = 0; i < col; i++)
+                for (int j = 0; j < col; j++)
+                    matrR[i, j] = 0;
+
+            for (int i = 0; i < col; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    if (Convert.ToBoolean(matrinc[j, i]))
+                    {
+                        for (int l = 0; l < col; l++)
+                            if (i != l && Convert.ToBoolean(matrinc[j, l])) matrR[i, l] = 1;
+                    }
+            }
+
+            for (int i = 0; i < col; i++)
+            {
+                for (int j = 0; j < col; j++)
+                    R.AppendText(matrR[i, j].ToString() + " ");
+                R.AppendText("\n");
+            }
+        }
+        private int[,] createVR(int[,] matr,int col, int n)
+        {
+            int[,] matrinc = new int[n, col];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < col; j++)
+                    matrinc[i, j] = 0;
+            int k = 0;
+            for (int i = 0; i < n; i++)
+                for (int j = i + 1; j < n; j++)
+                    if (Convert.ToBoolean(matr[i, j]))
+                    {
+                        matrinc[i, k] = 1;
+                        matrinc[j, k] = 1;
+                        k++;
+                    }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < col; j++)
+                    VR.AppendText(matrinc[i, j].ToString() + " ");
+                VR.AppendText("\n");
+            }
+            return matrinc;
+        }
+        private void DFS(bool[] vis,int v,int[,] g)
+        {
+            vis[v] = true;
+
+
+
+
+            for (int i = 0; i < vis.Length; i++)
+            {
+                if (!vis[i] && Convert.ToBoolean(g[v, i]))DFS(vis, v, g);
+            }
+        }
+            private void button1_Click(object sender, EventArgs e)
+        {
+            bool f1, f2, f3, f4;
+            f1 = Convert.ToBoolean(V.Text.Length);
+            f2 = Convert.ToBoolean(R.Text.Length);
+            f3 = Convert.ToBoolean(VR.Text.Length);
+            f4 = Convert.ToBoolean(L.Text.Length);
+            if (f1) prV();
+            if (f2) ;
+            if (f3) prVR();
+            if (f4) prL();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            V.Clear();
+            R.Clear();
+            VR.Clear();
+            L.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel) return;
+            string filename= openFileDialog1.FileName;
+            string filetext = System.IO.File.ReadAllText(filename);
+            V.Text = filetext;
+        }
+    }
+}
